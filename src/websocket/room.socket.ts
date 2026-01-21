c92969546ba7867dde755eb171f4b3f1239bdf19
Bibek <npbibek94@gmail.com>
2026-01-21 18:56:35 +0545

    refactor: remove old uppercase Share/utils files from git index (case normalization)

--- src/websocket/room.socket.ts
+++
@@ -1,9 +0,0 @@
-import { Server, Socket } from "socket.io";
-
-export const roomSocket = (io: Server) => {
-  io.on("connection", (socket: Socket) => {
-    socket.on("leave-room", (roomId: string) => {
-      socket.leave(roomId);
-    });
-  });
-};

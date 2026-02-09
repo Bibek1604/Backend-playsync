816264e8b836c96f750588e9af5df796b67fef30
Bibek <npbibek94@gmail.com>
2026-02-09 08:16:58 +0545

    feat: Integrate game module with main app and start cleanup job

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import path from "path";
 
 import authRoutes from "./modules/auth/auth.routes";
 import profileRoutes from "./modules/profile/profile.routes";
+import gameRoutes from "./modules/game/game.routes";
 
 import logger from "./Share/utils/logger";
 const app = express();
@@ -70,6 +71,7 @@ app.use(
 
 app.use(`${API_BASE}/auth`, authRoutes);
 app.use(`${API_BASE}/profile`, profileRoutes);
+app.use(`${API_BASE}/games`, gameRoutes);
 
 app.get("/", (_req, res) => {
   res.send(
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import http from "http";
 import app from "./app";
 import connectDB from "./Share/config/db";
 import logger from "./Share/utils/logger";
+import gameCleanupJob from "./jobs/game.cleanup.job";
 
 
 
@@ -21,6 +22,9 @@ const PORT = process.env.PORT || 5000;
     // Seed Admin
     const { seedAdmin } = await import("./modules/auth/auth.seeder");
     await seedAdmin();
+
+    // Start game cleanup cron job
+    gameCleanupJob.start();
 
     const server = http.createServer(app);
 

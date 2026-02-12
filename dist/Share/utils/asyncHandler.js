1298bd90a3919dfcf188edf27ce6b7105232527c
Bibek <npbibek94@gmail.com>
2026-02-13 01:11:55 +0545

    feat: implemention of admin panel

--- dist/Share/utils/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = void 0;
-const asyncHandler = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-};
-exports.asyncHandler = asyncHandler;
-exports.default = exports.asyncHandler;
-//# sourceMappingURL=asyncHandler.js.map
